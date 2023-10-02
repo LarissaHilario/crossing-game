2851b6c2320d947e7721e6694be30343424fd47e
Larissa Hilario Clemente <larihid1@gmail.com>
2023-10-01 23:17:26 -0600

    feat:add 3 goroutines

--- scenes/game_over.go
+++ scenes/game_over.go
@@ -1,1 +1,26 @@
 package scenes
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+type GameOverScene struct {
+    app    fyne.App
+    window fyne.Window
+}
+
+func NewGameOverScene() *GameOverScene {
+    return &GameOverScene{
+        app:    app.New(),
+        window: app.New().NewWindow("Game Over"),
+    }
+}
+
+func (gos *GameOverScene) Show() {
+    gameOverLabel := widget.NewLabel("Game Over")
+    gos.window.SetContent(container.NewVBox(gameOverLabel))
+    gos.window.Show()
+}
--- scenes/game_scene.go
+++ scenes/game_scene.go
@@ -1,23 +1,22 @@
-
 package scenes
 
 import (
-	"fmt"
+    "fmt"
+    "time"
 
-	"time"
-
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
-	"juego-pollo/models"
-	"juego-pollo/utils"
+    "fyne.io/fyne/v2"
+    "fyne.io/fyne/v2/app"
+    "fyne.io/fyne/v2/container"
+    "fyne.io/fyne/v2/widget"
+    "juego-pollo/models"
+    "juego-pollo/utils"
 )
+
 const (
-    gameWidth     = 900
-    gameHeight    = 500
-    playerSize    = 50
-    obstacleSize  = 50
+    gameWidth    = 900
+    gameHeight   = 500
+    playerSize   = 50
+    obstacleSize = 50
 )
 
 type GameScene struct {
@@ -27,20 +26,20 @@ type GameScene struct {
     obstacle    *models.Obstacle
     score       int
     scoreLabel  *widget.Label
-	statusLabel *widget.Label
+    statusLabel *widget.Label
     gameObjects fyne.CanvasObject
 }
 
 func NewGameScene() *GameScene {
     gameScene := &GameScene{
-        app:    app.New(),
-        window: app.New().NewWindow("Crossy Road"),
-        player: models.NewPlayer(),
-        obstacle: models.NewObstacle(),
-        scoreLabel: widget.NewLabel("Score: 0"),
-		statusLabel: widget.NewLabel("vas bien"),
+        app:         app.New(),
+        window:      app.New().NewWindow("Crossy Road"),
+        player:      models.NewPlayer(),
+        obstacle:    models.NewObstacle(),
+        scoreLabel:  widget.NewLabel("Score: 0"),
+        statusLabel: widget.NewLabel("vas bien"),
     }
-	gameScene.statusLabel.Move(fyne.NewPos(50,460))
+    gameScene.statusLabel.Move(fyne.NewPos(50, 460))
     gameScene.window.Resize(fyne.NewSize(gameWidth, gameHeight))
     gameScene.player.MoveTo(gameWidth/2-playerSize/2, gameHeight-playerSize-10)
     gameScene.gameObjects = container.NewWithoutLayout(gameScene.player.GetRectangle(), gameScene.obstacle.GetRectangle(), gameScene.scoreLabel, gameScene.statusLabel)
@@ -48,51 +47,45 @@ func NewGameScene() *GameScene {
     return gameScene
 }
 
+func movePlayer(gs *GameScene) {
+    for {
+        select {
+        case <-time.After(time.Millisecond * 100):
+            gs.player.MoveTo(gs.player.GetRectangle().Position().X, gs.player.GetRectangle().Position().Y-10)
+            if gs.player.GetRectangle().Position().Y < 0 {
+                gs.player.MoveTo(gs.player.GetRectangle().Position().X, gameHeight-playerSize-10)
+                gs.score++
+                gs.scoreLabel.SetText(fmt.Sprintf("Score: %d", gs.score))
+            }
+        }
+    }
+}
 
+func moveObstacle(gs *GameScene) {
+    for {
+        select {
+        case <-time.After(time.Millisecond * 100):
+            gs.obstacle.MoveTo(gs.obstacle.GetRectangle().Position().X+10,0)
+        }
+    }
+}
 
+func checkCollision(gs *GameScene) {
+    for {
+        select {
+        case <-time.After(time.Millisecond * 100):
+            if utils.ImageOverlaps(gs.player.GetRectangle(), gs.obstacle.GetRectangle()) {
+                gs.statusLabel.SetText("¡Ay te aplastaron!")
+                return
+            }
+        }
+    }
+}
 
 func (gs *GameScene) Start() {
-
-    go func() {
-        for {
-            select {
-            case <-time.After(time.Millisecond * 100):
-                gs.player.MoveTo(gs.player.GetRectangle().Position().X, gs.player.GetRectangle().Position().Y-10)
-                if gs.player.GetRectangle().Position().Y < 0 {
-                    gs.player.MoveTo(gs.player.GetRectangle().Position().X, gameHeight-playerSize-10)
-                    gs.score++
-                    gs.scoreLabel.SetText(fmt.Sprintf("Score: %d", gs.score))
-					
-                }
-            }
-        }
-    }()
-
-    go func() {
-        for {
-            select {
-            case <-time.After(time.Millisecond * 100):
-		
-                gs.obstacle.MoveTo(gs.obstacle.GetRectangle().Position().X+10, gs.obstacle.GetRectangle().Position().Y)
-			  }
-				
-				if utils.ImageOverlaps(gs.player.GetRectangle(), gs.obstacle.GetRectangle()) {
-			
-				gs.statusLabel.SetText("Game Over!")
-			
-			return
-            }
-        }
-    }()
-
-	go func() {
-		if utils.ImageOverlaps(gs.player.GetRectangle(), gs.obstacle.GetRectangle()) {
-			gs.statusLabel.SetText("Game Over!")
-			return
-		}
-			
-		}()
-
+    go movePlayer(gs)
+    go moveObstacle(gs)
+    go checkCollision(gs)
 
     gs.window.ShowAndRun()
 }
